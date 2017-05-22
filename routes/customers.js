b1fc916fb17c159c0343cbb70c72c59fdc1249e3
yparedes <yussel_paredes@hotmail.com>
2017-05-22 02:21:45 -0500

    Adds create customer endpoint

--- routes/customers.js
+++ routes/customers.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var conekta = require('../models/conekta');
 
-/* GET home page. */
+// Find customer by id
 router.get('/:customer_id', function(req, res, next) {
     conekta.Customer.find(req.params.customer_id, function(err, customer) {
         if (err) {
@@ -12,5 +12,39 @@ router.get('/:customer_id', function(req, res, next) {
         res.send(customer.toObject());
     });
 });
+
+// Create a customer
+router.post('/', function(req, res, next) {
+    conekta.Customer.create({
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone
+    }, function(err, customer) {
+        if (err) {
+            res.status(err.http_code).send(err.details);
+            return;
+        }
+        res.send(customer.toObject());
+    });
+});
+
+// Remove customer
+// /customers/:customer_id			DELETE
+
+
+// Add a card to a customer
+// /customers/:customer_id/card			POST
+
+// Remove a card from a customer
+// /customers/:customer_id/card/:card_id	DELETE
+
+
+
+
+// Add a subscription to a customer
+// /customers/:customer_id/subscription		POST
+
+// Cancel customer’s subscription
+// /customers/:customer_id/subscription/cancel	POST
 
 module.exports = router;
