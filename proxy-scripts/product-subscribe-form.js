e77c1c93260567ae09793fb30ab66f0a2a2dcee5
yparedes <yussel_paredes@hotmail.com>
2017-05-29 01:11:11 -0500

    Modified subscribe button

--- proxy-scripts/product-subscribe-form.js
+++ proxy-scripts/product-subscribe-form.js
@@ -1,10 +1,37 @@
 ;(function () {
+    var product;
 
-    var subscribeForm = $('<form></form>');
+    try {
+        product = JSON.parse($('script#ProductJson-product-template').text());
+    } catch (e) {}
 
-    subscribeForm.append('<select></select>');
+    console.log(product);
 
-    $('#shopify-section-product-template')
-        .find('form[action="/cart/add"]')
-        .after(subscribeForm);
+    if (!product) return;
+
+    $.get('/admin/products/'+product.id+'/metafields.json?namespace=ayma-recurring&key=isSubscriptionActive')
+        .done(function (data) {
+            var isSubscriptionActive;
+            console.log(data.metafields);
+            if (!data || !data.metafields || data.metafields.length === 0) return;
+
+            isSubscriptionActive = !!data.metafields[0].value;
+
+            console.log(isSubscriptionActive);
+
+            if (!isSubscriptionActive) return;
+
+            // create subscribe form
+            //var subscribeForm = $('<form></form>');
+
+            //subscribeForm.append('<button type="submit" class="btn">Suscribirse</button>');
+
+            //$('#shopify-section-product-template')
+            //    .find('form[action="/cart/add"]')
+            //    .after(subscribeForm);
+
+            $('#shopify-section-product-template')
+                .find('form[action="/cart/add"]')
+                .append('<button type="submit" class="btn">Suscribirse</button>');
+        });
 }());
--- routes/recurring-charges.js
+++ routes/recurring-charges.js
@@ -8,6 +8,11 @@ router.get('/install', RecurringChargesCtrl.install);
 
 router.get('/auth', RecurringChargesCtrl.auth);
 
+/*router.get('/product-subscribe-form.js', function (req, res) {
+    res.header('Content-Type', 'application/liquid');
+    res.send('{{product.id}}');
+});*/
+
 router.use(express.static('proxy-scripts'));
 
 module.exports = {
